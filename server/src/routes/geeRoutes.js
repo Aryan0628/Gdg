ecdd61c1bc38545ddd90405a97b6725d5d6af6a9
Aryangupta <aryan072806@gmail.com>
2026-01-04 01:24:56 +0530

    added middleware in gee routes

--- server/src/routes/geeRoutes.js
+++ server/src/routes/geeRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import axios from "axios";
+import { checkJwt } from "../auth/authMiddleware.js";
 const router=express.Router()
 import {generatefireReport } from "../controllers/viirs.js";
 import {generateFloodReport} from "../controllers/sentinel1.js";
@@ -7,11 +7,11 @@ import { generateLandHeatReport } from "../controllers/landsat8_9.js";
 import {generateDeforestationReport} from "../controllers/copernicus.js"
 import {generatePollutantsReport} from "../controllers/sentinel5p.js"
 import {generateCoastalReport} from "../controllers/landsat.js"
-router.post('/generatefireReport',generatefireReport)
-router.post('/generateFloodReport',generateFloodReport)
-router.post('/generateLandHeatReport',generateLandHeatReport)
-router.post('/generateDeforestationReport',generateDeforestationReport)
-router.post('/generatePollutantsReport',generatePollutantsReport)
-router.post('/generateCoastalReport',generateCoastalReport)
+router.post('/generatefireReport',checkJwt,generatefireReport)
+router.post('/generateFloodReport',checkJwt,generateFloodReport)
+router.post('/generateLandHeatReport',checkJwt,generateLandHeatReport)
+router.post('/generateDeforestationReport',checkJwt,generateDeforestationReport)
+router.post('/generatePollutantsReport',checkJwt,generatePollutantsReport)
+router.post('/generateCoastalReport',checkJwt,generateCoastalReport)
 
-export default router
+export default router;
